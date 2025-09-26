3df5d6a9a08758085eaca543af6a7119fd73a9fe
quanlaura <quanhs2006@gmail.com>
2025-09-27 00:54:22 +0700

    intro

---
+++ HW1/Introduction.java
@@ -0,0 +1,11 @@
+public class Introduction {
+  public class void main (String[] argv) {
+    System.out.print("Tran Minh Quan" + "\t" + "K69I-IT6" + "\t" + "24020285" 
+                     + "\t" + "quanlaura" + "quanhs2006@gmail.com" + "\n");
+    for (int i >= 1; i = 20; i--) {
+      System.out.println ( i + "bottles of beer on the wall, " +i + " bottles of beer." + "\n"
+                          + "Take one down, pass it around,");
+     }
+    System.out.println("No more bottles of beer on the wall.");
+  }
+}  
