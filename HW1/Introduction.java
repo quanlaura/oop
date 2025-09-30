ca0d38b5f12a45fb9f65a1ea8ba6973b5387e67a
quanlaura <quanhs2006@gmail.com>
2025-09-30 21:27:17 +0700

    Update Introduction.java

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -1,5 +1,5 @@
 public class Introduction {
-  public class void main (String[] args) {
+  public static void main (String[] args) {
     System.out.print("Tran Minh Quan" + "\t" + "K69I-IT6" + "\t" + "24020285" 
                      + "\t" + "quanlaura" + "quanhs2006@gmail.com" + "\n");
     for (int i = 9; i >= 1; i--) {
